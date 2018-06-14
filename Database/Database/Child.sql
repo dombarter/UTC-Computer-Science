b566c5304f44aff063b3ad5ff77a282dd447b327
Dom Barter <dominicbarter@gmail.com>
2018-06-14 10:24:42 +0100

    added a few tables

---
+++ Database/Database/Child.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Child]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ Database/Database/Parent.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Parent]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(MAX) NULL, 
+    [Age] NVARCHAR(MAX) NULL
+)
